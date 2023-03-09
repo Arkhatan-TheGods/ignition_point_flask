09afefc9560f7ee59e7c728db3cf4ec78bd3744b
Arkhatan-TheGods <pauloevertron@outlook.com>
2023-03-08 21:19:33 -0300

    mudando para o app.run() motivo: fase de desenvolvimento

--- main/app.py
+++ main/app.py
@@ -2,10 +2,17 @@ from flask import Flask
 from waitress import serve
 
 app = Flask(__name__)
+print('nome do módulo', __name__)
+# escreva as rotas aqui
 
-#escreva as rotas aqui
+
+@app.route('/')
+def hello():
+    return {'message': " Buddy."}
+
 
 # aqui inicia o app e o servidor
 if __name__ == "__main__":
-    # comando para rodar via powershell: waitress-serve --host=127.0.0.1 --port=8080 tests.app:app
-    serve(app, _quiet=True, host='127.0.0.1', port=8080, url_scheme='https')
+    # comando para rodar via powershell: waitress-serve --host=127.0.0.1 --port=8080 main.app:app
+    # serve(app, _quiet=True, host='127.0.0.1', port=8080, url_scheme='https', _profile=True)
+    app.run(debug=True, host='127.0.0.1', port=8080)
