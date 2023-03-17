a235c361de6168bba38309536a7109bcb20df9be
Arkhatan-TheGods <pauloevertron@outlook.com>
2023-03-17 12:10:54 -0300

    simple commmit

---
+++ main/infra/db/tables.py
@@ -0,0 +1,17 @@
+from sqlite3 import Connection, connect
+import database_query
+def build_tables(data_base: str):
+
+    conn: Connection | None = None
+
+    try:
+        conn = connect(data_base)
+        cursor = conn.cursor()
+        cursor.execute(database_query.create_table_costumer())
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        if conn:
+            conn.close()
