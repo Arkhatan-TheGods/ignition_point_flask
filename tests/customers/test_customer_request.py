b2bd59edb77a8183fa9e8e39e53044f4f3b6bd7d
rodrigmars <rodrigmars@gmail.com>
2023-03-20 13:33:01 -0300

    Drop e Create table implementados no módulo de teste test_customer_request.py

--- tests/customers/test_customer_request.py
+++ tests/customers/test_customer_request.py
@@ -1,3 +1,4 @@
+from sqlite3 import Connection, Cursor, connect
 from re import match
 from pytest import mark, fixture
 from requests import get, post
@@ -5,30 +6,65 @@ import json
 
 
 @fixture
-def setup():
+def setup() -> tuple:
 	# TODO: implementar versionamento /api/v1/
-	
-	return 'http://127.0.0.1:8080/customers/', {
-		"name": "Yago André Almada",
-		"cpf": "942.554.492-10",
-		"birth_date": "27/01/2002",
-		"address": "Quadra 12 Conjunto F, 311"
-	}
+
+	conn: Connection = connect("database")
+
+	cursor: Cursor = conn.cursor()
+
+	cursor.executescript("""
+	BEGIN;
+		
+		DROP TABLE IF EXISTS CUSTOMERS;
+		DROP TABLE IF EXISTS PRODUCTS;
+		
+		CREATE TABLE IF NOT EXISTS CUSTOMERS(
+                ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                NAME TEXT NOT NULL,
+                CPF TEXT NOT NULL UNIQUE,
+                BIRTH_DATE TEXT NOT NULL,
+                ADDRESS TEXT NOT NULL,
+                CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+
+            CREATE TABLE IF NOT EXISTS PRODUCTS(
+                ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                NAME TEXT NOT NULL,
+                CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+            COMMIT;""")
+
+	cursor.close()
+
+	conn.close()
+
+	return ('http://127.0.0.1:8080/customers/',
+         [{"name": "Yago André Almada",
+           "cpf": "942.554.492-10",
+           "birth_date": "27/01/2002",
+           "address": "Quadra 12 Conjunto F, 311"},
+          {"name": "Maitê Daiane da Mata",
+           "cpf": "648.487.491-31",
+           "birth_date": "23/01/1969",
+           "address": "Rua Manoel Severino da Silva, 303"},
+          {"name": "Nicolas Danilo Leonardo da Rocha",
+           "cpf": "219.581.745-30",
+           "birth_date": "04/01/1945",
+           "address": "Conjunto Residencial 1 Condomínio 1 Bloco C, 568"}])
 
 
-def test_only_portuguese_words_with_space(setup: tuple):
+def test_only_portuguese_words_with_space(setup: tuple) -> None:
 
 	pattner_name = "^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ\\s]+$"
 
-	assert match(pattner_name, setup[1]["name"])
+	assert match(pattner_name, setup[1][0]["name"])
 
-
-# @mark.skip(reason="")
-def test_post_customer_200(setup: tuple):
+def test_post_customer_200(setup: tuple) -> None:
 
 	uri, customer = setup
+	
 
-	input_data = json.dumps(customer)
+	input_data = json.dumps(customer[1])
 
 	headers = {'Content-Type': 'application/json'}
 
@@ -37,13 +73,17 @@ def test_post_customer_200(setup: tuple):
 	assert 200 == response.status_code
 
 
-@mark.skip(reason="")
-def test_get_all_customers(setup):
+# @mark.skip(reason="")
+def test_get_all_customers(setup) -> None:
 
-	uri, _ = setup
+	uri, customer = setup
+
+	input_data = json.dumps(customer[1])
+
+	headers = {'Content-Type': 'application/json'}
+
+	post(uri, input_data, headers=headers)
 
 	response = get(uri)
 
-	print("response:", response)
-
-	# assert "942.554.492-10" == response.json()['cpf']
+	assert [] != response
