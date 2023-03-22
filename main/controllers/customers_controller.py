49a41cf85234f7be3c1f23a8cd012d672c92b281
Arkhatan-TheGods <pauloevertron@outlook.com>
2023-03-22 20:00:52 -0300

    criado função de atualizar customer

--- main/controllers/customers_controller.py
+++ main/controllers/customers_controller.py
@@ -3,9 +3,9 @@ from flask import request
 from utils.enum_utils import TypeException
 
 
-def customers_controller(controller: Callable) -> dict:
+def customers_controller(controller_decorator: Callable) -> dict:
 
-    @controller
+    @controller_decorator
     def create(services: dict) -> tuple:
 
         if request.headers['Content-Type'] != 'application/json':
@@ -16,30 +16,42 @@ def customers_controller(controller: Callable) -> dict:
 
         services["add"](customer)
 
-        return "Cliente cadastrado com sucesso", 200
+        return "Cliente cadastrado com sucesso", 201
 
-    @controller
+    @controller_decorator
     def get_customers(services: dict) -> tuple:
 
         return {"customers": services["all"]()}, 200
 
-    @controller
+    @controller_decorator
     def get_by_id(services: dict, **kwargs: dict) -> tuple:
 
         return {"customer": services["get_by_id"]((kwargs.get("customer_id"),))}, 200
 
-    @controller
+    @controller_decorator
     def get_by_cpf(services: dict, **kwargs: dict) -> tuple:
 
         return {"customer": services["get_by_cpf"]((kwargs.get("cpf"),))}, 200
 
-    @controller
+    @controller_decorator
     def get_by_name(services: dict, **kwargs: dict) -> tuple:
     
         return {"customer": services["get_by_name"]((kwargs.get("name"),))}, 200
+
+    @controller_decorator
+    def update_by_id(services: dict, **kwargs: dict):
+
+        if request.headers['Content-Type'] != 'application/json':
+            raise Exception(TypeException.CUSTOMER,
+                            {"error": 'Header Error'}, 415)
+
+        customer = tuple(request.get_json().values())
+
+        return {"customer": services["update_by_id"]((kwargs.get("costumer_id"),))}, 202
 
     return {'create': create,
             'all': get_customers,
             'get_by_id': get_by_id,
             'get_by_cpf': get_by_cpf,
-            'get_by_name': get_by_name}
+            'get_by_name': get_by_name,
+            'update_by_id': update_by_id}
--- main/routes/routing.py
+++ main/routes/routing.py
@@ -12,13 +12,10 @@ from services.service import get_service
 from infra.dependency_injector import container
 
 
-def routing(app: Flask,
-            data_base: str) -> None:
+def routing(app: Flask, data_base: str) -> None:
 
     args = get_service, data_base, container
 
-    register_customer_routes(app, customers_controller(
-        controller_decorator("CUSTOMER", args)))
+    register_customer_routes(app, customers_controller(controller_decorator("CUSTOMER", args)))
 
-    register_product_routes(app, products_controller(
-        controller_decorator("PRODUCT", args)))
+    register_product_routes(app, products_controller(controller_decorator("PRODUCT", args)))
